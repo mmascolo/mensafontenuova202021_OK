ef6fb1747287843632f545bc1085586194c3a67c
Michele <michele.mascolo@gmail.com>
2019-05-31 17:09:14 +0200

    GOOD

--- app/src/main/java/com/mminf/mensafontenuova/Menu1.java
+++ app/src/main/java/com/mminf/mensafontenuova/Menu1.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.webkit.ValueCallback;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -22,53 +24,149 @@ import android.widget.TextView;
 
 
 public class Menu1 extends Fragment {
+    Button button;
+    Button button2;
+    Button button3;
+    Button button4;
+
     public void scrivi_str(String campo, String valore) {
-        SharedPreferences mPreferences_agg = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SharedPreferences mPreferences_agg = PreferenceManager.getDefaultSharedPreferences(getContext());
         mPreferences_agg.edit().putString(campo, valore).commit();
     }
 
-
-
     public void scrivi_int(String campo, int valore) {
-        SharedPreferences mPreferences3 = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SharedPreferences mPreferences3 = PreferenceManager.getDefaultSharedPreferences(getContext());
         mPreferences3.edit().putInt(campo, valore).commit();
     }
 
     public String leggi_str(String campo) {
-        SharedPreferences mPreferences_leg = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SharedPreferences mPreferences_leg = PreferenceManager.getDefaultSharedPreferences(getContext());
         String valore = "";
         return mPreferences_leg.getString(campo, valore);
     }
 
     public String leggi_sito(String campo, String valore) {
-        SharedPreferences mPreferences_leg = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SharedPreferences mPreferences_leg = PreferenceManager.getDefaultSharedPreferences(getContext());
         return mPreferences_leg.getString(campo, valore);
     }
 
 
     public int leggi_int(String campo) {
-        SharedPreferences mPreferences_leg = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SharedPreferences mPreferences_leg = PreferenceManager.getDefaultSharedPreferences(getContext());
         int valore = 0;
         return mPreferences_leg.getInt(campo, valore);
     }
 
+    public void addListenerOnButton(View view) {
+
+        button = (Button) view.findViewById(R.id.CAL1);
+        button2 = (Button) view.findViewById(R.id.CAL2);
+        button3 = (Button) view.findViewById(R.id.CAL3);
+        button4 = (Button) view.findViewById(R.id.CAL4);
+
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                scrivi_str("calendario", "4");
+                Log.e("clic", "click");
+                menu_saldo_sub calendario = new menu_saldo_sub();
+                FragmentManager manager = getFragmentManager();
+                manager.beginTransaction().replace(R.id.content_frame, calendario, getClass().getName()).commit();
+            }
+
+        });
+
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                scrivi_str("calendario", "3");
+                Log.e("clic", "click");
+                menu_saldo_sub calendario = new menu_saldo_sub();
+                FragmentManager manager = getFragmentManager();
+                manager.beginTransaction().replace(R.id.content_frame, calendario, getClass().getName()).commit();
+            }
+
+        });
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                scrivi_str("calendario", "2");
+                Log.e("clic", "click");
+                menu_saldo_sub calendario = new menu_saldo_sub();
+                FragmentManager manager = getFragmentManager();
+                manager.beginTransaction().replace(R.id.content_frame, calendario, getClass().getName()).commit();
+            }
+        });
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Log.e("clic", "click");
+                scrivi_str("calendario", "1");
+                menu_saldo_sub calendario = new menu_saldo_sub();
+                FragmentManager manager = getFragmentManager();
+                manager.beginTransaction().replace(R.id.content_frame, calendario, getClass().getName()).commit();
+            }
+
+        });
+
+    }
+
+
 
     @Nullable
+
+
+//        menu_saldo_sub calendario = new menu_saldo_sub();
+
+    //      FragmentManager manager = getFragmentManager();
+
+    //    manager.beginTransaction().replace(R.id.content_frame,calendario ).commit();
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
         View v = inflater.inflate(R.layout.fragment_menu_2, container, false);
+        addListenerOnButton(v);
+
+        button = (Button) v.findViewById(R.id.CAL1);
+        button2 = (Button) v.findViewById(R.id.CAL2);
+        button3 = (Button) v.findViewById(R.id.CAL4);
+        button4 = (Button) v.findViewById(R.id.CAL3);
+
+        button.setVisibility(getView().INVISIBLE);
+        button2.setVisibility(getView().INVISIBLE);
+        button3.setVisibility(getView().INVISIBLE);
+        button4.setVisibility(getView().INVISIBLE);
+
+
+
+
+
+
 
 
         TextView bambino = v.findViewById(R.id.textView2);
         TextView bambino2 = v.findViewById(R.id.textView4);
 
-            bambino.setVisibility(getView().VISIBLE);
+
+        bambino.setVisibility(getView().VISIBLE);
             bambino2.setVisibility(getView().VISIBLE);
             bambino2.setText("Per visualizzare il saldo attuale");
             bambino.setText("Impostare le credenziali nelle impostazioni");
 
+        String username_sito = leggi_str("username");
+        String password_sito = leggi_str("password");
+        scrivi_int("bambini", -1);
 
 
 
@@ -82,10 +180,6 @@ public class Menu1 extends Fragment {
             webSettings.setJavaScriptEnabled(true);
             myWebView.getSettings().setDomStorageEnabled(true);
             myWebView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
-            String username_sito = leggi_str("username");
-            String password_sito = leggi_str("password");
-
-
             String url = "https://www.schoolesuite.it/default1/NSC_Login.aspx?installation_code=fontenuopre";
             final String js = "javascript: document.getElementById('txtUsername').value='" + username_sito + "';" + "document.getElementById('txtPassword').value='" + password_sito + "';" + "document.getElementById('btnOK').click()";
             myWebView.loadUrl(url);
@@ -104,28 +198,30 @@ public class Menu1 extends Fragment {
 
                     if (url.contains("login")) {
                         TextView bambino = getView().findViewById(R.id.textView2);
-                        bambino.setText("errore login");
+                        bambino.setText("Errore login controllare login");
+                        scrivi_str("connesso", "NO");
                     }
-                    if (url.contains("PWM_ChildrenList.aspx")) {
+
+                    if ((url.contains("PWM_ChildrenList.aspx"))) {
+
+                        int a = leggi_int("bambini");
 
 
-                        view.evaluateJavascript("(function() { return (document.getElementById('tblChildrenList').rows.length); })();", new ValueCallback<String>() {
+                        if (a < 0) {
+                            view.evaluateJavascript("(function() { return (document.getElementById('tblChildrenList').rows.length); })();", new ValueCallback<String>() {
 
-                                    @Override
-                                    public void onReceiveValue(String html21) {
+                                        @Override
+                                        public void onReceiveValue(String html21) {
 //                    i= Integer.parseInt(html21);
 
-                                        scrivi_int("bambini", Integer.parseInt(html21));
-                                        Log.e("Contalinee", html21);
-                                        try {
-                                            Thread.sleep(1000);
-                                        } catch (InterruptedException e) {
-                                            // TODO Auto-generated catch block
-                                            e.printStackTrace();
+                                            scrivi_int("bambini", Integer.parseInt(html21));
+                                            Log.e("Contalinee", html21);
+                                            myWebView.reload();
+
                                         }
                                     }
-                                }
-                        );
+                            );
+                        }
 
                         int contarow = leggi_int("bambini");
 
@@ -147,6 +243,23 @@ public class Menu1 extends Fragment {
                                     TextView bambino = getView().findViewById(R.id.textView4);
                                     bambino.setText(input);
                                     bambino.setVisibility(getView().VISIBLE);
+
+                                    bambino.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+
+
+                                            menu_saldo_sub calendario = new menu_saldo_sub();
+
+                                            FragmentManager manager = getFragmentManager();
+
+                                            manager.beginTransaction().replace(R.id.content_frame, calendario, calendario.getTag());
+
+                                        }
+                                    });
+
+
+
                                 }
                             });
 
@@ -162,6 +275,7 @@ public class Menu1 extends Fragment {
                                             TextView saldo = getView().findViewById(R.id.textView2);
                                             saldo.setText(input2);
                                             saldo.setVisibility(getView().VISIBLE);
+                                            button.setVisibility(getView().VISIBLE);
 
 
                                         }
@@ -207,6 +321,7 @@ public class Menu1 extends Fragment {
                                             TextView saldo = getView().findViewById(R.id.textView2);
                                             saldo.setText(input2);
                                             saldo.setVisibility(getView().VISIBLE);
+                                            button.setVisibility(getView().VISIBLE);
                                         }
                                     });
 
@@ -242,7 +357,7 @@ public class Menu1 extends Fragment {
                                             TextView saldo2 = getView().findViewById(R.id.textView7);
                                             saldo2.setVisibility(getView().VISIBLE);
                                             saldo2.setText(input2);
-
+                                            button2.setVisibility(getView().VISIBLE);
 
                                         }
                                     });
@@ -291,6 +406,7 @@ public class Menu1 extends Fragment {
                                             TextView saldo = getView().findViewById(R.id.textView2);
                                             saldo.setVisibility(getView().VISIBLE);
                                             saldo.setText(input2);
+                                            button.setVisibility(getView().VISIBLE);
                                         }
                                     });
 
@@ -326,6 +442,8 @@ public class Menu1 extends Fragment {
                                             TextView saldo2 = getView().findViewById(R.id.textView7);
                                             saldo2.setVisibility(getView().VISIBLE);
                                             saldo2.setText(input2);
+                                            button2.setVisibility(getView().VISIBLE);
+
                                         }
                                     });
 
@@ -359,7 +477,7 @@ public class Menu1 extends Fragment {
                                             TextView saldo2 = getView().findViewById(R.id.textView9);
                                             saldo2.setText(input2);
                                             saldo2.setVisibility(getView().VISIBLE);
-
+                                            button3.setVisibility(getView().VISIBLE);
 
                                         }
                                     });
@@ -405,7 +523,7 @@ public class Menu1 extends Fragment {
                                             TextView saldo = getView().findViewById(R.id.textView2);
                                             saldo.setVisibility(getView().VISIBLE);
                                             saldo.setText(input2);
-
+                                            button.setVisibility(getView().VISIBLE);
 
                                         }
                                     });
@@ -442,6 +560,7 @@ public class Menu1 extends Fragment {
                                             TextView saldo2 = getView().findViewById(R.id.textView7);
                                             saldo2.setVisibility(getView().VISIBLE);
                                             saldo2.setText(input2);
+                                            button2.setVisibility(getView().VISIBLE);
                                         }
                                     });
 
@@ -475,6 +594,7 @@ public class Menu1 extends Fragment {
                                             TextView saldo2 = getView().findViewById(R.id.textView9);
                                             saldo2.setVisibility(getView().VISIBLE);
                                             saldo2.setText(input2);
+                                            button3.setVisibility(getView().VISIBLE);
                                         }
                                     });
 
@@ -509,7 +629,7 @@ public class Menu1 extends Fragment {
                                             TextView saldo2 = getView().findViewById(R.id.textView12);
                                             saldo2.setVisibility(getView().VISIBLE);
                                             saldo2.setText(input2);
-
+                                            button4.setVisibility(getView().VISIBLE);
 
                                         }
                                     });
@@ -531,10 +651,22 @@ public class Menu1 extends Fragment {
         }
 
 
-
         return v;
 
     }
+ /*   public void myMethod(View view) {
+        TextView clikme = getView().findViewById(R.id.textView4);
+        Log.e ("aaa","clicked!");
+        menu_saldo_sub calendario = new menu_saldo_sub();
+
+        FragmentManager manager = getFragmentManager();
+
+        manager.beginTransaction().replace(R.id.content_frame,calendario );
+
+    }
+*/
+
+
 
 
     @Override
